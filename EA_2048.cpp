ada90956dfb95fddfd3aac8eda0dfbef364569b6
Pedro Tiago <72353485+pedrotiag0@users.noreply.github.com>
2021-03-03 16:52:12 +0000

    Update EA_2048.cpp

--- EA_2048.cpp
+++ EA_2048.cpp
@@ -12,10 +12,24 @@
 
 using namespace std;
 
-vector<int> swipeLeft() {
+/*
+vector<int> swipeLeft(vector<int> tabuleiro) {
 
 }
+*/
 
+void imprimeTabuleiro(vector<int> tabuleiro, int N) {
+    int paragrafo = 0;
+    cout << "_______________________" << endl;
+    for (int i = 0; i < N*N ; i++) {
+        if (paragrafo == N) {
+            paragrafo = 0;
+            cout << endl;
+        }
+        cout << tabuleiro[i] <<" ";
+    }
+    cout << endl <<"_______________________" << endl;
+}
 
 string jogo_2048() {
 
@@ -27,21 +41,35 @@ int main()
     // Ler de maneira eficiente
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    vector<int> tabuleiro_2048;             // Tabuleiro do jogo
+
     vector<string> solution;                        // Solucao
 
-    int total;                                      // numero de casos de teste
+    vector<int> tabuleiro_2048;                     // Tabuleiro do jogo
+    string tabuleiro_line;                          // Linha a ser lida
+    size_t pos = 0;
+    string delimiter = " ";
+
+    int total;                                      // Numero de casos de teste
     cin >> total;
 
-    unsigned short int N, M;
+    unsigned short int N = 0, M;
 
     for (int i = 0; i < total ; i++) {              // Le e processa cada caso de teste
+        cout << "Nova leitura!" << endl;
         cin >> N >> M;
+        cin.ignore();
+        tabuleiro_2048.clear();
         for (int j = 0; j < N; j++) {
-                                                    // Le linha de jogo (que estruturas de dados vamos usar?, criar arrays (ficam na stack) ou vetores (heap e sao dinamicos))
+            getline(cin, tabuleiro_line);
+            while ((pos = tabuleiro_line.find(delimiter)) != std::string::npos) {
+                tabuleiro_2048.push_back(stoi(tabuleiro_line.substr(0, pos)));
+                tabuleiro_line.erase(0, pos + delimiter.length());
+            }
+            tabuleiro_2048.push_back(stoi(tabuleiro_line));
+            // Le linha de jogo (que estruturas de dados vamos usar?, criar arrays (ficam na stack) ou vetores (heap e sao dinamicos))
         }
-        solution.push_back(jogo_2048());
+        //solution.push_back(jogo_2048());
+        imprimeTabuleiro(tabuleiro_2048, N);
     }
 
     for (auto i : solution) {                        //Imprime output
