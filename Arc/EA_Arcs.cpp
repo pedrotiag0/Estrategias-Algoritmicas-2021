aa1f765f6b14d3196c7c3e9d6224d1cd7098ffae
Pedro Tiago <72353485+pedrotiag0@users.noreply.github.com>
2021-04-21 17:39:23 +0100

    21-04-2021

--- Arc/EA_Arcs.cpp
+++ Arc/EA_Arcs.cpp
@@ -38,13 +38,92 @@ int mod_sub(int a, int b, int mod) {
     return mod_add(a, -b, mod);
 }
 
+bool possivelDescer(int altura, int pos) { // Retorna true se for possivel descer e tocar no solo
+    int restantes = n - (pos + 1);
+    int melhorCaso = altura - (restantes * (h - 1));
+    return melhorCaso <= h;
+    /*
+    if (melhorCaso == 0) // Deve descer de imediato
+        return 1;
+    else if (melhorCaso < 0) // Pode continuar ou entao descer
+        return 0;
+    else
+        return -1; // Caso invalido que nao deve ser contabilizado
+    */
+}
+
+int* calculaLimiares() {
+    // Funcao que calcula os valores min e maximo que podemos subir e tocar (ou nao) no teto, contando que é sempre possível retornar ao solo.
+    int limiares[2];
+
+    // Limiar Inferior
+    int alturaAtual = h;
+    int i;
+    for (i = 1; i < n; i++) {
+        if ( (alturaAtual + (h - 1) < H) && (possivelDescer(alturaAtual + (h - 1), i))) {
+            alturaAtual += h - 1;
+        }
+        else {
+            i--;
+            break;
+        }  
+    }
+
+    for (int j = 1; j < h; j++) { // Verifica se consegue "tocar" no teto
+        if ((alturaAtual + j == H) && (possivelDescer(alturaAtual + j, i + 1))) {
+            i++;
+            break;
+        }
+    }
+
+    limiares[0] = i;
+
+    // Limiar Superior
+    int auxLimiar = n - limiares[0];
+    alturaAtual = h;
+    for (i = 1; i < n; i++) {
+        if ((alturaAtual + 1 <= H) && (possivelDescer(alturaAtual + 1, i))) {
+            alturaAtual++;
+        }
+        else {
+            i--;
+            break;
+        }
+    }
+
+    limiares[1] = min(auxLimiar, i);
+
+    cout << "Limiar Inferior: " << limiares[0] << endl;
+    cout << "Limiar Superior: " << limiares[1] << endl;
+
+    return limiares;
+}
+
+int calculaPossibilidades(int i, int j) {
+    // i--> Largura atual | j --> Altura target atual
+    int auxPossibilidades = 0;
+
+    // ...
+
+    return auxPossibilidades;
+}
+
 void arc(int* sala) {
     /*for (int i = 0; i < n; i++) {
         buildArc(sala, i, true);
     }*/
     //buildArc(sala, 0, true);
-    int aux;
-    for (int i = 2; i < n; i++) {           // Todos os casos possiveis
+    int auxPossibilidades, alturaAtual, difAltura;
+    
+    for (int i = 3; i <= n; i++) {
+        for (int j = h; j <= H; j++) {
+            auxPossibilidades = calculaPossibilidades(i, j);
+            possibilidades = mod_add(possibilidades, auxPossibilidades, MODULO);
+        }
+    }
+
+    // [Casos Possiveis - Casos Desfavoraveis]
+    /*for (int i = 2; i < n; i++) {                   // Todos os casos possiveis
         aux = (int)(pow((h-1), (i-1)));
         //cout << "AUX1: " << aux << endl;
         //possibilidades = mod_add(possibilidades, aux, MODULO);
@@ -53,14 +132,15 @@ void arc(int* sala) {
 
     // Falta distinguir quando fazemos elevado a h-1, h-2, h-3... Ou seja, validar a altura restante que conseguimos subir.
     
-    for (int i = 2; i < n; i++) {           // Subtrai a todos os casos possiveis os casos onde ultrapassa a altura maxima
-        for (int j = 1; j < h ; j++) {      // Contar para todos os tamanhos do degrau que ultrapassam H
-            aux = (int)(pow(j, (i - 1)));   // Por alterar
+    for (int i = 2; i < n; i++) {                   // Subtrai a todos os casos possiveis os casos onde ultrapassa a altura maxima
+        for (int j = 1; j < h ; j++) {              // Contar para todos os tamanhos do degrau que ultrapassam H
+            aux = (int)(pow(j, (i - 1)));           // Por alterar
             //cout << "AUX2: " << aux << endl;
             //possibilidades = mod_sub(possibilidades, aux, MODULO);
             possibilidades -= aux;
         }
-    }
+    }*/
+
 }
 
 int main() {
@@ -84,7 +164,8 @@ int main() {
                 sala[j] = -1;
             }
             possibilidades = 0;
-            arc(sala);
+            //arc(sala);
+            calculaLimiares();
             solution.push_back(mod_abs(possibilidades, MODULO));
         }
         else
